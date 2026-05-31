0ee9e39fd33b5c16e24fd5a702d5d29592584ed3
Chuannnn1 <chuannnn@github.com>
2026-06-01 01:44:25 +0800

    fix: mvnw.cmd properly detect java on PATH or JAVA_HOME (not just file existence)

--- mvnw.cmd
+++ mvnw.cmd
@@ -13,7 +13,6 @@ set "WRAPPER_PROPS=%MAVEN_PROJECTBASEDIR%\.mvn\wrapper\maven-wrapper.properties"
 
 if not exist "%WRAPPER_JAR%" (
     echo Error: maven-wrapper.jar not found at %WRAPPER_JAR% 1>&2
-    echo Please run: git submodule update --init 1>&2
     exit /b 1
 )
 if not exist "%WRAPPER_PROPS%" (
@@ -21,20 +20,40 @@ if not exist "%WRAPPER_PROPS%" (
     exit /b 1
 )
 
-@REM Find Java
+@REM Find Java: try JAVA_HOME first, then fall back to PATH
+set "JAVA_EXE="
 if defined JAVA_HOME (
     set "JAVA_EXE=%JAVA_HOME%\bin\java.exe"
-) else (
-    set "JAVA_EXE=java"
+    if exist "!JAVA_EXE!" goto :run_maven
+    echo [WARN] JAVA_HOME is set but java.exe not found at !JAVA_EXE! 1>&2
+    set "JAVA_EXE="
 )
 
-if not exist "%JAVA_EXE%" (
-    echo Java not found. JAVA_HOME=%JAVA_HOME% 1>&2
-    echo Please install JDK 17+ and set JAVA_HOME 1>&2
-    exit /b 1
+@REM Try to find java on PATH
+for /f "delims=" %%J in ('where java 2^>nul') do (
+    set "JAVA_EXE=%%J"
+    goto :run_maven
 )
 
-@REM Run MavenWrapperMain using maven-wrapper.jar
-@REM The wrapper jar contains MavenWrapperMain which downloads and runs Maven
-"%JAVA_EXE%" -jar "%WRAPPER_JAR%" %*
+@REM Last resort: check common install locations
+if exist "C:\Program Files\Eclipse Adoptium\jdk-17*\bin\java.exe" (
+    for /f "delims=" %%J in ('dir /b "C:\Program Files\Eclipse Adoptium\jdk-17*\bin\java.exe" 2^>nul') do (
+        set "JAVA_EXE=C:\Program Files\Eclipse Adoptium\%%J"
+        goto :run_maven
+    )
+)
+if exist "C:\Program Files\Java\jdk-17*\bin\java.exe" (
+    for /f "delims=" %%J in ('dir /b "C:\Program Files\Java\jdk-17*\bin\java.exe" 2^>nul') do (
+        set "JAVA_EXE=C:\Program Files\Java\%%J"
+        goto :run_maven
+    )
+)
+
+echo Java not found. Please install JDK 17+ and ensure java is on PATH 1>&2
+echo JAVA_HOME=%JAVA_HOME% 1>&2
+exit /b 1
+
+:run_maven
+@REM Run MavenWrapperMain - it handles downloading Maven automatically
+"!JAVA_EXE!" -jar "%WRAPPER_JAR%" %*
 exit /b %errorlevel%
